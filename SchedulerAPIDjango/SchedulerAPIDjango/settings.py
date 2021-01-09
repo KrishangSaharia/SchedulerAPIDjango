1680ab1bcc1ce5ff9aefe5c1b08b8361c6f27b09
Krishang <krishangsaharia2@gmail.com>
2021-01-09 17:59:06 +0530

    added files

--- SchedulerAPIDjango/SchedulerAPIDjango/settings.py
+++ SchedulerAPIDjango/SchedulerAPIDjango/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+import django_heroku
+django_heroku.settings(locals())
